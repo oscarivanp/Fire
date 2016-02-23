50bff427b28225446e39b4880d75278ba807236d
rafaelmartinez <oscarivan_p@hotmail.com>
2016-02-23 16:09:30 -0500

    Initial commit

--- app/src/main/java/com/rmasc/fireroad/IntroActivity.java
+++ app/src/main/java/com/rmasc/fireroad/IntroActivity.java
@@ -1,9 +1,9 @@
 package com.rmasc.fireroad;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
@@ -19,6 +19,7 @@ public class IntroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+//cambios
 
         AjustarInterface();
     }
