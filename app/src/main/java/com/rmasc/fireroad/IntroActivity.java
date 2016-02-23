b24790d7cf2b670b473ea6597fb5a341a2f387cd
diegobernal <diego@rmasc.com>
2016-02-23 16:37:43 -0500

    Initial commit

--- app/src/main/java/com/rmasc/fireroad/IntroActivity.java
+++ app/src/main/java/com/rmasc/fireroad/IntroActivity.java
@@ -19,7 +19,7 @@ public class IntroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
-//cambios ya2erw
+//cambios ya2erw otro
 
         AjustarInterface();
     }
